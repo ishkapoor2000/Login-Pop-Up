ae664f567b54231d277b92fb4892bff349a76963
Ish Kapoor <ishkapoor2000@gmail.com>
2021-03-27 20:50:19 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const loginPopup = document.querySelector(".login-popup");
+const close = document.querySelector(".close");
+
+window.addEventListener("load", function () {
+	showPopup();
+})
+
+function showPopup() {
+	const timeLimit = 5;
+	let i = 0;
+	const timer = setInterval(function () {
+		i++;
+		if(i==timeLimit){
+			clearInterval(timer);
+			loginPopup.classList.add("show");
+		}
+		console.log(i);
+	}, 1000);
+}
+
+close.addEventListener("click", function(){
+	loginPopup.classList.remove("show");
+});
