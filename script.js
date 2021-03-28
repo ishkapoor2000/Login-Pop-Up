bf9da7e3b8f192ed129beca7a7c537007f3af96f
Ish Kapoor <60035881+ishkapoor2000@users.noreply.github.com>
2021-03-28 16:57:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const loginPopup = document.querySelector(".login-popup");
 const close = document.querySelector(".close");
 
+const subBtn = document.querySelector(".btn");
+
 window.addEventListener("load", function () {
 	showPopup();
 })
@@ -19,5 +21,9 @@ function showPopup() {
 }
 
 close.addEventListener("click", function(){
+	loginPopup.classList.remove("show");
+});
+
+subBtn.addEventListener("click", function(){
 	loginPopup.classList.remove("show");
 });
